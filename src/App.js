608a9e771620a4942da0b582e2608fcc9e6eeaec
Othmane elkhiari <othmaneelkhiari@gmail.com>
2023-02-21 22:04:38 +0100

    Handle Looding

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ import {
   WhatsappIcon,
 } from "react-share";
 function App() {
-  
+  const [islooding,setIsLooding] = useState(false)
   const [data,setData] = useState([
     {
         "quote": "I think the key to happiness is allowing ourselves to not feel bad or guilty for feeling it, and letting it be contagious. And to not be dependent on other people to create your own happiness.",
@@ -31,6 +31,7 @@ function App() {
     HandleClick()
   },[])
   const HandleClick = ()=>{
+    setIsLooding(true)
     var config = {
       method: 'get',
     maxBodyLength: Infinity,
@@ -43,9 +44,11 @@ function App() {
     axios(config)
     .then(function (response) {
       setData(response.data);
+    setIsLooding(true)
     })
     .catch(function (error) {
       console.log(error);
+    setIsLooding(false)
     });
   }
   return (
@@ -53,7 +56,7 @@ function App() {
       <header className="App-header bg-gray-100">
         <div className='p-3  rounded flex place-content-center place-items-center'>
             
-        <figure className="max-w-screen-md mx-auto text-center   rounded p-10 shadow-md cursor-pointer active:bg-gray-50 bg-white" onClick={HandleClick}>
+        {!islooding?<div className='italic font-medium max-w-screen-md mx-auto text-center   rounded p-10 shadow-md cursor-pointer active:bg-gray-50 bg-white text-black'>Looding...</div>:<figure data-popover-target="popover-top" data-popover-placement="top" className="max-w-screen-md mx-auto text-center   rounded p-10 shadow-md cursor-pointer active:bg-gray-50 bg-white" onClick={HandleClick}>
             <svg aria-hidden="true" className="w-12 h-12 mx-auto mb-3 text-gray-400 dark:text-gray-600 " viewBox="0 0 24 27" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M14.017 18L14.017 10.609C14.017 4.905 17.748 1.039 23 0L23.995 2.151C21.563 3.068 20 5.789 20 8H24V18H14.017ZM0 18V10.609C0 4.905 3.748 1.038 9 0L9.996 2.151C7.563 3.068 6 5.789 6 8H9.983L9.983 18L0 18Z" fill="currentColor"/></svg>
             <blockquote>
                 <p className="text-2xl italic font-medium text-gray-900 dark:text-white">"{data && data[0].quote}"</p>
@@ -64,9 +67,19 @@ function App() {
                     <cite className="pl-3 text-sm font-light text-gray-500 dark:text-gray-400">{data && data[0].category}</cite>
                 </div>
             </figcaption>
-        </figure>
+        </figure>}
+        <div data-popover id="popover-top" role="tooltip" class="absolute z-10 invisible inline-block w-64 text-sm font-light text-gray-500 transition-opacity duration-300 bg-white border border-gray-200 rounded-lg shadow-sm opacity-0 dark:text-gray-400 dark:border-gray-600 dark:bg-gray-800">
+            <div class="px-3 py-2 bg-gray-100 border-b border-gray-200 rounded-t-lg dark:border-gray-600 dark:bg-gray-700">
+                <h3 class="font-semibold text-gray-900 dark:text-white">Information</h3>
+            </div>
+            <div class="px-3 py-2">
+                <p>Click me for generate Quotes</p>
+            </div>
+            <div data-popper-arrow></div>
+        </div>
 
       <figcaption className="flex items-center justify-center mt-6 space-x-3 absolute bottom-2">
+                <img class="w-6 h-6 rounded-full" src="https://firebasestorage.googleapis.com/v0/b/steam-circlet-374115.appspot.com/o/images%2F%2B212%20684-149860%2020221010_225154.jpg?alt=media&token=89a8bd9a-bf70-439a-84d3-ef0fd8568ca6" alt="profile picture" />
                 <div className="flex items-center divide-x-2 divide-gray-500 dark:divide-gray-700">
                     <cite className="pr-3 font-medium text-gray-900 dark:text-white">Othmane Elkhiari<elkhiari></elkhiari></cite>
                     <cite className="pl-3 text-sm font-light text-gray-500 dark:text-gray-400"><a href="https://github.com/elkhiari">Github</a></cite>
